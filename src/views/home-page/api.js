e7ba9adf2dcda66faa0f8f1cba507e1315f0b385
张赛 <you@example.com>
2018-07-31 20:16:12 +0800

    提交

---
+++ src/views/home-page/api.js
@@ -0,0 +1,6 @@
+/**
+ * Created by yyr on 2018/2/8.
+ */
+export const api = {
+
+}
---
+++ src/views/home-page/index.vue
@@ -0,0 +1,28 @@
+/**
+* Created by yyr on 2018/2/8首页
+*/
+<template>
+    <div>
+
+    </div>
+</template>
+<script type="text/ecmascript-6">
+    export default {
+        data() {
+            return {
+
+            }
+        },
+        mounted() {
+        },
+        methods: {
+
+        },
+        components: {
+
+        }
+    }
+</script>
+<style rel="stylesheet/scss" lang="scss" scoped>
+
+</style>
